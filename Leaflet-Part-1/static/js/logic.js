2029d99af28ffd2fb24fbc9655739b31bdf714fd
Barsha Basel <barsha_basel432@hotmail.com>
2023-07-04 23:49:06 +1000

    marker

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -2,66 +2,41 @@
 
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// fetching the geojson data
-
-d3.json(queryUrl).then(function(data) {
-  console.log(data);
-  createMarkers(data.features);
-});
-
-// Add a tile to the map = a background. Comes from OpenStreetmap
-let  earthquakeMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-    maxZoom: 6,
-    });
-
-
 // intializing the map object 
 let map = L.map('map', {
-  center: [47, 2],
+  center: [47, 7],
   zoom: 12,
-  layers: [earthquakeMap]
   });   // center position + zoom
 
-
-// create a baseMaps objects to hold the streetmap layer
-
-let baseMaps = {
-  "EartheQuake": earthquakeMap
-
-};
-
-// create a layer control, and pass it baseMaps and overlayMap. Adding the layers to the map
-
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// create an overlaysMaps object to hold the magnitude marker layers
-let overlayMaps = {};
+// fetching the geojson data
 
-function createMarkers(data){
+d3.json(queryUrl).then(function(response) {
 
-  // pulling the cordinates from the features
-  let magnitude = data.geometry.coordinates;
+  console.log(response);
+  features = response.features;
 
-  // Initialize an array to hold magnitude markers
-  let magMarkers =[];
+  let magnitudes = [];
 
-  // looping through the array.
-  for(let i=0; i < data.length; i++){
-    let magnitudes = magnitude[i];
-     
-    let magMarker = L.marker([magnitudes[0], magnitudes[1]])
-                     .bindPopup("<h3> Status: " + features.properties.status + "</h3>");
+  for (let i = 0; i < features.length; i++) {
+    let location = features[i].geometry;
+    if (location) {
+      //console.log(location);
+      magnitudes.push([location.coordinates[0], location.coordinates[1]]);
+    }
 
-    magMarkers.push(magMarker);
   }
 
-// Add the magMarkers layer to overlayMaps
-overlayMaps.Magnitude = L.layerGroup(magMarkers);
+  let layer = L.marker(magnitudes).addTo(map);
 
-}
+});
+
+
+
 
 
 
