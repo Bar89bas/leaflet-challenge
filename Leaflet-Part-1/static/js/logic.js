716d6e7e1fbed88fc395f06ddfa3f3940b8b151f
Barsha Basel <barsha_basel432@hotmail.com>
2023-07-05 20:46:12 +1000

    final submission

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,43 +1,117 @@
-// intializing the url for data collection
+// Adding the tile layer
 
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
 
-// intializing the map object 
+// initializing variable for map
+
 let map = L.map('map', {
-  center: [47, 7],
-  zoom: 12,
+  center: [40.7, -94.5],
+  zoom: 3,
   });   // center position + zoom
 
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// adding background to the map
+baseMap.addTo(map);
 
-// fetching the geojson data
+// intializing the url 
 
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+// fetching data from query url
 d3.json(queryUrl).then(function(response) {
+  
+  // function for circle radius and color based on the earquake magnitude and depth
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
 
-  console.log(response);
-  features = response.features;
+  // function to fill circle color
+  function getColor(depth){
 
-  let magnitudes = [];
-
-  for (let i = 0; i < features.length; i++) {
-    let location = features[i].geometry;
-    if (location) {
-      //console.log(location);
-      magnitudes.push([location.coordinates[0], location.coordinates[1]]);
+    switch (true) {
+      case depth > 90:
+        return "#ea2c2c";
+      case depth > 70:
+        return "#ea822c";
+      case depth > 50:
+        return "#ee9c00";
+      case depth > 30:
+        return "#eecc00";
+      case depth > 10:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
     }
 
   }
 
-  let layer = L.marker(magnitudes).addTo(map);
+  // function for circle radius
+  function getRadius(magnitude) {
+    if (magnitude === 0){
+      return 1;
+    } 
+    return magnitude *4;
+  }
 
+  
+  // Add geojson data into layer to map
+  L.geoJson(response, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
+      );
+    }
+  }).addTo(map);
+
+  // initializing variable to add legend to the map
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // function to add legend features 
+  legend.onAdd=function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let grades = [-10, 10, 30, 50, 70, 90];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+    // looping throuth the grades 
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+
+  }
+
+  // adding legend to the map
+  legend.addTo(map);
 });
-
-
-
-
-
-
 
